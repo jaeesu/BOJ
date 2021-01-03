5703c7fc92e0fab0b5c11bd1ba7333893ec1b457
jaeesu <wogmlsu@gmail.com>
2021-01-03 20:38:19 +0900

    Back Tracking 3

---
+++ 15651.py
@@ -0,0 +1,17 @@
+visited = [False]*9
+out=[]
+
+def finder(cnt, N, M) :
+    if cnt==M :
+        print(' '.join(map(str, out)))
+        return
+    for i in range(N) :
+        if not visited[i] :
+            visited[i] = True
+            out.append(i+1)
+            visited[i] = False
+            finder(cnt+1, N, M)
+            out.pop()
+
+N,M=map(int, input().split())
+finder(0, N, M)
