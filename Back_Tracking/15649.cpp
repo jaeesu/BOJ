e34d56be334b8f68e067709b159edfecf3d49ac7
jaeesu <wogmlsu@gmail.com>
2021-01-03 21:22:48 +0900

    Back Tracking 1

---
+++ Back_Tracking/15649.cpp
@@ -0,0 +1,20 @@
+/*#include<iostream>
+using namespace std;
+
+const int LIMIT 9;
+
+void search(int cnt){
+    for(int i=1;i<=N;i++){
+        if(visited[i]==0){
+            visited[i]++;
+            cnt--;
+            search(cnt);
+            visited[i]--;
+        }
+    }
+    
+}
+
+int main(){
+    return 0;
+}*/
