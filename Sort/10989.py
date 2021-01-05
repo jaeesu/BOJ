18c4daa83db46fcb551077c319fe1dca525ca32b
jaeesu <wogmlsu@gmail.com>
2021-01-05 23:00:09 +0900

    Sorting 3 : Counting Sort

--- Sort/10989.py
+++ Sort/10989.py
@@ -1,14 +1,13 @@
 # counting sort : 계수 정렬
 
 import sys
-N = int(sys.stdin.readline())
+N = int(input())
 
 arr = [0]*10001
 
 for i in range(N) :
     arr[int(sys.stdin.readline())] += 1
     
-print("-------------------")
 for i in range(len(arr)) :
     while(arr[i]!=0) :
         print(i)
