fb8cf2b6a04b2a250b03f3413bce6ed0b0675164
jaeesu <wogmlsu@gmail.com>
2021-01-05 22:31:37 +0900

    Sorting 4 : Statics

--- Sort/2108.py
+++ Sort/2108.py
@@ -5,18 +5,18 @@ def mod(arr) :
     if(len(arr)==1) :
         return arr[0]
     else :
-        cnt = Counter(list).most_common()
+        cnt = Counter(arr).most_common()
         if(cnt[0][1]==cnt[1][1]) :
             return cnt[1][0]
         else :
             return cnt[0][0]
 
 
-list=[]
+lst=[]
 N=int(input())
-for i in range(N) : list.append(int(input()))
-list.sort()
-print(round(sum(list)/len(list)))
-print(list[int((N-1)/2)])
-print(mod(list))
-print(max(list)-min(list))
+for i in range(N) : lst.append(int(input()))
+lst.sort()
+print(round(sum(lst)/len(lst)))
+print(lst[int((N-1)/2)])
+print(mod(lst))
+print(max(lst)-min(lst))
