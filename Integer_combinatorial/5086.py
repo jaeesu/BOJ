256698495ff97a6cd21d69528d02986a89daee0a
jaeesu <wogmlsu@gmail.com>
2021-01-06 21:06:04 +0900

    Integer_combinatorial 1 : factor_multiple

--- Integer_combinatorial/5086.py
+++ Integer_combinatorial/5086.py
@@ -1,6 +1,6 @@
 A,B = map(int, input().split())
 
-while (A!=0 and B!=0) :
+while (A!=0 and B!=0):
     if(B%A==0) : print("factor")
     elif(A%B==0) : print("multiple")
     else : print("neither")
