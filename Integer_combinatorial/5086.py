fa6731fcb6cb29dd6b8a82eeaf4d3176e11562b7
jaeesu <wogmlsu@gmail.com>
2021-01-06 21:04:55 +0900

    factor and multiple

---
+++ Integer_combinatorial/5086.py
@@ -0,0 +1,7 @@
+A,B = map(int, input().split())
+
+while (A!=0 and B!=0) :
+    if(B%A==0) : print("factor")
+    elif(A%B==0) : print("multiple")
+    else : print("neither")
+    A,B = map(int, input().split())
