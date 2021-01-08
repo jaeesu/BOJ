4ed0fd3d3054c64390faa5e7f3de903f7e993f8a
jaeesu <wogmlsu@gmail.com>
2021-01-08 19:52:11 +0900

    Stack 4 : a balanced world

---
+++ Stack/4949.py
@@ -0,0 +1,28 @@
+import sys
+
+def check(word, lst) :
+    if(word=="(" or word=="[") :
+        lst.append(word)
+    elif(word==")") :
+        if (len(lst)<=0 or lst[-1]!="(") :
+            return False
+        lst.pop()
+    elif(word=="]") :
+        if (len(lst)<=0 or lst[-1] != "[") :
+            return False
+        lst.pop()
+    return True
+
+while(1) :
+    global stk
+    stk=[]
+    line=sys.stdin.readline()[:-1]
+    if(line==".") : break
+    for word in line :
+        if(word==".") : 
+            if(check(word,stk)==True) :
+                print("yes")
+                break
+        if (check(word, stk)==False) :
+            print("no")
+            break
