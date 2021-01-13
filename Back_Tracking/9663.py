fbb7101da17fca1f103aec4922adfcbb9bf81f86
jaeesu <wogmlsu@gmail.com>
2021-01-13 21:16:11 +0900

    b_t

---
+++ Back_Tracking/9663.py
@@ -0,0 +1,16 @@
+#N-Queen
+#어디로 가더라도 말이 있는 상황
+#모든 말이 서로 대각선이나 같은 열에 있어야 함
+
+count = 0
+
+def dfs(cnt) :
+    global count
+    if cnt == N :
+        count+=1
+        return
+    for i in range(N) :
+        return
+
+
+N = map(int, input())
