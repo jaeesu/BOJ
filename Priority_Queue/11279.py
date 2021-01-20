97528ae7846d1dac5e3a09798fb5cdcbebfabbb8
jaeesu <wogmlsu@gmail.com>
2021-01-20 23:31:08 +0900

    Priority_Queue 1 : Max_Heap

---
+++ Priority_Queue/11279.py
@@ -0,0 +1,14 @@
+#max_heap
+import sys
+import heapq as heap
+
+n=int(sys.stdin.readline())
+lst=[]
+for i in range(n) :
+    x = int(sys.stdin.readline())
+    heap.heappush(lst, (-x,x))
+    if(x==0) :
+        if len(lst) > 0 :
+            print(heap.heappop(lst)[1])
+        else :
+            print(0)
